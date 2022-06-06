90abd9e076dfe8bbd0e9eb6a7f13ec81d4797786
Zhenyu Yang <50858833+yzyjsjem@users.noreply.github.com>
2022-06-06 17:31:34 +1000

    create project

---
+++ weather app/weather app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather app
+//
+//  Created by 杨振誉 on 6/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weather app/weather app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather app
+//
+//  Created by 杨振誉 on 6/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
